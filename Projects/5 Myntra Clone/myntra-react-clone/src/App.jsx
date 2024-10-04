c5ea3e929fe5304d565a0aa7f4089d73dfa8cd62
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-10-04 17:12:27 +0530

    Basic Structure

---
+++ Projects/5 Myntra Clone/myntra-react-clone/src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="items-container"></div>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
